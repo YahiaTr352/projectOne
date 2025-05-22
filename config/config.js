08fe7f2e28bce15f8d57a74b13f0c13c9dedf258
YahiaTr352 <yahiatr188@gmail.com>
2025-05-22 20:09:37 +0300

    final  commit

--- config/config.js
+++ config/config.js
@@ -1,35 +1,36 @@
-// const mongoose = require("mongoose");
-
-// const ConnectDB = async () => {
-//   try {
-//     await mongoose.connect("mongodb://localhost:/PaymentGateWay");
-//     console.log("✅ Connected to MongoDB");
-
-//   } catch (error) {
-//     console.error("❌ Failed to connect to MongoDB:", error);
-//   }
-// };
-
-// module.exports = ConnectDB;
-
-
 const mongoose = require("mongoose");
-const uri = "mongodb+srv://user1:yahiamo99@cluster0.ymmjl1j.mongodb.net/";
-const ConnectDB = async() => {
 
-    try{
-    await mongoose.connect(uri, {
-        ssl: true, // تأكد من أن الاتصال باستخدام SSL
-    })
+const ConnectDB = async () => {
+  try {
+    await mongoose.connect("mongodb+srv://user1:yahiamo99@cluster0.pzmvgfj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    console.log("✅ Connected to MongoDB");
 
-    console.log("connected to mongodb");
-
-    }catch(error){
-        console.log("failed to connect to mongoDB");
-        console.log("..............................................");
-        console.log(error);
-    }
-}
-
+  } catch (error) {
+    console.error("❌ Failed to connect to MongoDB:", error);
+  }
+};
 
 module.exports = ConnectDB;
+
+
+// const mongoose = require("mongoose");
+// const uri = "mongodb+srv://user1:yahiamo99@cluster0.34d4ikz.mongodb.net/";
+
+// const ConnectDB = async() => {
+
+//     try{
+//      await mongoose.connect(uri, {
+//         ssl: true, // تأكد من أن الاتصال باستخدام SSL
+//     })
+
+//     console.log("connected to mongodb");
+
+//     }catch(error){
+//         console.log("failed to connect to mongoDB");
+//         console.log("..............................................");
+//         console.log(error);
+//     }
+// }
+
+
+// module.exports = ConnectDB;
